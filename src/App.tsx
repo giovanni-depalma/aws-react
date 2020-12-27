6cd609b28d0ddec8d1ba295151a19d45036e2906
giovanni.depalma <giovanni.depalma@algowatt.com>
2020-12-27 10:49:24 +0100

    test edit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React V 2.0
         </a>
       </header>
     </div>
