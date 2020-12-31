6f38a6a78289a17ec22238e55192a6c8b42de31d
giovanni.depalma <giovanni.depalma@algowatt.com>
2020-12-31 12:36:40 +0100

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -19,8 +20,9 @@ function App() {
           Learn React V 2.0
         </a>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
